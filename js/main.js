a905e4a061165eaef24ef23620550a44dca99ba9
AmrHekal <amrhekal2030@gmail.com>
2024-12-07 12:38:59 +0200

    edit

--- js/main.js
+++ js/main.js
@@ -4,7 +4,9 @@ const list = document.querySelector(".app nav ul");
 bars.addEventListener("click", () => {
  if (list.clientHeight == 0) {
   list.style.height = `${list.scrollHeight}px`;
+  bars.style.transform = "rotate(-45deg)";
  } else {
   list.style.height = "";
+  bars.style.transform = "";
  }
 });
